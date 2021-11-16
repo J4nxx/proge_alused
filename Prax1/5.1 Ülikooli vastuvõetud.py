a14decf33093766173ab3607f80a6d951537b175
Janre Liiv <janreliiv@gmail.com>
2021-11-16 16:56:54 +0200

    5.1 ülikooli vastuvõetud

---
+++ Prax1/5.1 Ülikooli vastuvõetud.py
@@ -0,0 +1,16 @@
+fail = open("rebased.txt", encoding="UTF-8")
+aasta = int(input("Palun sisestage, millise aasta andmeid vajate: "))
+aasta1 = 2011
+if aasta <= 2010 or aasta >= 2020:
+        print("Kahjuks ei ole meil informatsiooni sellel aastal.")
+        
+for rida in fail:
+    if aasta == aasta1:
+        print(aasta1, " aastal oli vastuvõetuid ", rida)
+        break
+    aasta1 +=1
+
+
+    
+    
+fail.close()
